1a23cfa695a41885edfd5cbff6f6d3f6854cc52b
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-03-26 23:47:52 -0300

    Adiciona Mais Questões

---
+++ exercícios/Python/Ex01/HelloWorld.py
@@ -0,0 +1,14 @@
+ # MODIFICANDO O HELLO WORLD
+ # Artur Rodrigues Santos
+
+ # 1. Altere seu programa para imprimir uma mensagem diferente.
+
+ # 2. Altere seu programa para imprimir duas linhas de texto usando duas linhas de código print.
+
+ # 3. Sabendo que os caracteres \n representam uma quebra de linhas, imprima duas linhas de texto usando uma única
+ # linha de código print.
+
+def main():
+	print("Olá, esse é o exercício 1!!!\nOlha só, outra linha, que bacana!!!")
+
+main()
