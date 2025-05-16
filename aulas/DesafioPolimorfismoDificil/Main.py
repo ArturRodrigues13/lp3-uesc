08b6515dcd992b8bd89fead0d4338e382b291c64
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-05-15 22:52:42 -0300

    eu sou burro!!!

--- aulas/DesafioPolimorfismoDificil/Main.py
+++ aulas/DesafioPolimorfismoDificil/Main.py
@@ -10,34 +10,39 @@ class Node:
 
 	def insert_after(self,data):
 		node = ComboNode(data)
+		node.prev = self
+		node.next = self.next
+		self.next.prev = node
+		self.next = node
 		return node
 
 	def delete(self):
-		self.prev = self.next
+		self.prev.next = self.next
+		self.next.prev = self.prev
 		return self.prev
 
 	def print_list(self):
 		node = self
-		prim = False
-		while(node != self or prim == False):
+		while True:
 			print(node.data)
 			node = node.next
-			prim = True
+			if(node == self):
+				print(node.data)
+				break
 
 
 class InsertNode(Node):
 	def insert_after(self,data):
 		node = super().insert_after(data)
-		node.prev = self
-		node.next = self.next
-		self.next = node
 		return node
 
 class DeleteNode(Node):
 	def delete(self):
 		node = super().delete()
-		node2 = node.next
-		node2.prev = node
+		if(node == self):
+			node.prev = node
+			node.next = node
+		return node
 
 class PrintNode(Node):
 	def print_list(self):
@@ -50,4 +55,6 @@ class ComboNode(InsertNode,DeleteNode,PrintNode):
 node = ComboNode(1)
 new_node = node.insert_after(2)
 new_node.insert_after(3)
-node.print_list()
+new_new_node = node.insert_after(10)
+new_new_node.prev.delete()
+new_new_node.print_list()
